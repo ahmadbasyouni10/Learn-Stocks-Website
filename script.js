90ea07c60291f849f801fb8a01a851bb0f8fa927
Ahmad Basyouni <120362910+ahmadbasyouni10@users.noreply.github.com>
2023-06-26 03:57:42 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const burger = document.querySelector("nav svg");
+
+burger.addEventListener("click", () => {
+  if (burger.classList.contains("active")) {
+    gsap.to(".links", { x: "100%" });
+    gsap.to(".line", { stroke: "white" });
+    gsap.set("body", { overflow: "auto" });
+    gsap.set("body", { overflowX: "hidden" });
+  } else {
+    gsap.to(".links", { x: "0%" });
+    gsap.to(".line", { stroke: "black" });
+    gsap.fromTo(
+      ".links a",
+      { opacity: 0, y: 0 },
+      { opacity: 1, y: 20, delay: 0.25, stagger: 0.25 }
+    );
+    gsap.set("body", { overflow: "hidden" });
+  }
+  burger.classList.toggle("active");
+});
+
+const videos = gsap.utils.toArray(".video");
+gsap.set(videos, { opacity: 0 });
+
+videos.forEach((video) => {
+  ScrollTrigger.create({
+    trigger: video,
+    start: "top center",
+    end: "bottom center",
+
+    onEnter: () => {
+      gsap.to(video, { opacity: 1 });
+      video.play();
+    },
+    onEnterBack: () => video.play(),
+    onLeave: () => video.pause(),
+    onLeaveBack: () => video.pause(),
+  });
+});
+const exploreCoursesButton = document.querySelector(".cta-sec");
+
+exploreCoursesButton.addEventListener("click", () => {
+  // Define the action to perform when the button is clicked
+  // For example, you can navigate to a different page using window.location.href
+  window.location.href = "https://www.tradingacademy.com/";
+});
